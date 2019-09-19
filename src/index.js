532ff08227ad1e600183614aedd1d8d8b4dbfcdb
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-19 09:57:37 +0700

    Using eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import "@babel/polyfill";
-const http = require('http');
-const requestHandler = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+import '@babel/polyfill';
+import http from 'http';
+
+function requestHandler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!');
-};
+}
 const server = http.createServer(requestHandler);
 server.listen(8080);
